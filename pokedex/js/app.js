47cf262379aaa1394295dfc8b29baa9bbc568129
Paula Eduarda Maciel <padumaciel@gmail.com>
2021-05-21 23:00:40 -0300

    Atualizações no código do app.js

--- pokedex/js/app.js
+++ pokedex/js/app.js
@@ -6,27 +6,27 @@ async function fetchTypesAsync() {
 // #region Utilizando await
 
 /* 
-    Cada linha é aguarda pela resposta da requisição que é feita nela. Menos código, mais legível.
-    Linha 14: Aguarda resposta do servidor
-    Linha 15: Aguarda conversão da resposta do servidor (Os dados mesmo)
+Cada linha é aguarda pela resposta da requisição que é feita nela. Menos código, mais legível.
+Linha 14: Aguarda resposta do servidor
+Linha 15: Aguarda conversão da resposta do servidor (Os dados mesmo)
 */
 
 let response = await fetch("https://pokeapi.co/api/v2/type")
 let data = await response.json()
 
 pokemonTypes = data.results.map(function (type) {
-    return type.name
+return type.name
 });
 
 // #endregion
 
 // #region Utilizando then
 /*        
-    Código onde cada then depende diretamente do outro. Mais código, menos legível.
-    Em sala fizemos assim, com a diferença que aqui estamos adicionando os dados obtidos numa váriavel
-    Linha 33: Aguarda resposta do servidor e inicia a conversão em json
-    Linha 34: Aguarda dados já convertidos
-    Linha 39: Mostra algum erro
+Código onde cada then depende diretamente do outro. Mais código, menos legível.
+Em sala fizemos assim, com a diferença que aqui estamos adicionando os dados obtidos numa váriavel
+Linha 33: Aguarda resposta do servidor e inicia a conversão em json
+Linha 34: Aguarda dados já convertidos
+Linha 39: Mostra algum erro
 */
 
 // pokemonTypes = await fetch("https://pokeapi.co/api/v2/type")
@@ -51,19 +51,19 @@ async function fetchPokemonsAsync() {
 
 //Primeira opção de resolução:
 pokemonList = await fetch("https://borgesdn.github.io/pokedex-source/pokedex.json")
-    .then((resposta) => resposta.json())
-    .catch(error => { 
-        console.error(error);
-    });
+.then((resposta) => resposta.json())
+.catch(error => { 
+    console.error(error);
+});
 return pokemonList
 
 /* Segunda opção de resolução:
 await fetch("https://borgesdn.github.io/pokedex-source/pokedex.json")
-    .then((resposta) => resposta.json())
-    .then((resposta) => pokemonList = resposta)
-    .catch(error => { 
-        console.error(error);
-    });
+.then((resposta) => resposta.json())
+.then((resposta) => pokemonList = resposta)
+.catch(error => { 
+    console.error(error);
+});
 */
 /* Terceira opção de resolução:
 let resposta = await fetch("https://borgesdn.github.io/pokedex-source/pokedex.json")
@@ -78,20 +78,20 @@ async function getPokemon(id) {
 // https://pokeapi.co/api/v2/pokemon/(id recebido no parametro)
 
 let pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then((resposta) => resposta.json())
-    .catch((error) => {
-        console.log(error);
-        alert("Erro ao buscar pokemon")
-    })
+.then((resposta) => resposta.json())
+.catch((error) => {
+    console.log(error);
+    alert("Erro ao buscar pokemon")
+})
 return pokemon
 }
 
 function filterPokemon(name, type) {
 const filteredList = pokemonList.filter(pokemon => {
-    const searchName = new RegExp(name, 'i');
-    const checkName = searchName.test(pokemon.name);
-    const checkType = type.length == 0 ? true : pokemon.type.includes(type);
-    return checkName && checkType;
+const searchName = new RegExp(name, 'i');
+const checkName = searchName.test(pokemon.name);
+const checkType = type.length == 0 ? true : pokemon.type.includes(type);
+return checkName && checkType;
 })
 return filteredList;
 }
